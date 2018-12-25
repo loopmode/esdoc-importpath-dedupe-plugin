d7bb850fc9b144d2d5a77f1861de0d6ebb370fb6
Jovica Aleksic <jovica.aleksic@xailabs.de>
2018-12-25 11:50:06 +0100

    1.0.2 handle missing options object

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 1.0.2 (2018-12-25)
+- Fix case without given options object
+
 ## 1.0.1 (2018-12-25)
 - Add default value `['js']` for `"extensions"` option
 
--- src/Plugin.js
+++ src/Plugin.js
@@ -2,21 +2,26 @@ const fs = require('fs');
 
 class Plugin {
   onHandleDocs(ev) {
-    let extensions = ev.data.option.extensions
+    let extensions = ['js'];
+    if (ev.data.option && ev.data.option.extensions) {
+      extensions = ev.data.option.extensions;
+    }
+    if (typeof extensions === 'string') {
+      extensions = extensions.split(',');
+    }
+
+
+
     for (const doc of ev.data.docs) {
       if (!doc.importPath) continue;
- 
+
       const parts = doc.importPath.split('/');
       if (parts.length >= 2) {
-        const secondLast = parts[parts.length - 2];
-        const last = parts[parts.length - 1];
-        let isDupe = last === secondLast;
-        if (!isDupe && extensions) {
-          if (typeof extensions === 'string') {
-            extensions = extensions.split(',');
-          }
-          isDupe = extensions.some(extension => `${secondLast}.${extension}` === last);
-        }
+        const secondLastPart = parts[parts.length - 2];
+        const lastPart = parts[parts.length - 1];
+        const isDupe = lastPart === secondLastPart || (
+          extensions && extensions.some(ext => lastPart === `${secondLastPart}.${ext}`)
+        ); 
         if (isDupe) {
           doc.importPath = parts.slice(0, parts.length - 1).join('/');
         }
